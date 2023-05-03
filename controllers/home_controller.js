656872250eeae931f08f059351e0cf5ffd03a5c6
haardik12 <sh66825.hs@gmail.com>
2023-05-03 07:35:42 +0530

    'added-post-and-comments-model'

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,23 @@
-module.exports.home = function (req, res) {
-  return res.render('home.ejs', {
-    title: 'home',
-  })
+const Post = require('../models/post')
+
+module.exports.home = async function (req, res) {
+  // post.find({}) this finds every post within the object created earlier and fetch it and the call back function return the needed values on the screen
+  //   // this function is an example of without populating the user in the post object
+
+  //   Post.find({}, function (err, posts) {
+  //     return res.render('home.ejs', {
+  //       title: 'home',
+  //       posts: posts,
+  //     })
+  //   })
+  // console.log(req.cookies)
+
+  //populate the user of each post
+  let posts = await Post.find({}).populate('user').exec()
+  if (posts) {
+    return res.render('home', {
+      title: 'Home',
+      posts: posts,
+    })
+  }
 }
--- models/post.js
+++ models/post.js
@@ -9,7 +9,7 @@ const postSchema = new mongoose.Schema(
 
     user: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'users',
+      ref: 'User',
     },
   },
   {
